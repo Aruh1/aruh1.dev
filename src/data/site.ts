39f65e6dee28f312c31d8b512cd4bf737a37ebe9
Aruh1 <rizkypraschool@gmail.com>
2026-07-23 22:39:43 +0700

    update desc

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const site = {
     name: "Aruh1",
     title: "Aruh1",
     description:
-        "Hey there! I build open-source tools, terminal experiences, backend systems, and full-stack web apps.",
+        "Hey there! I build stuff, and full-stack web apps.",
     url: "https://aruh1.dev",
     language: "en",
     locale: "en_US",
